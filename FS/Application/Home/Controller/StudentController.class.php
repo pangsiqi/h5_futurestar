e68e938bfdac80253766c50ab8db1a5055a4787b
zhangzheng1019 <447590461@qq.com>
2016-12-06 08:53:10 +0800

    增加了前台学生版块的静态页，并让其显示

---
+++ FS/Application/Home/Controller/StudentController.class.php
@@ -0,0 +1,61 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class StudentController extends Controller {
+	public function index(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function changeinform(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function chinese(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function classgarden(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function classspace(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function english(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function gift(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function math(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function parentspace(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function personcenter(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function pk(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function studycontent(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+	public function walktolisten(){
+		layout(false); // 临时关闭当前模板的布局功能
+		$this->show();
+	}
+}
+
+
+
+?>
