f6d929bd8db0e9589796a61faf3147c2cfe58008
pangsiqi <746609995@qq.com>
2016-12-18 19:35:44 +0800

    暂时存储

--- FS_siqi/Application/Home/Controller/StudentController.class.php
+++ FS_siqi/Application/Home/Controller/StudentController.class.php
@@ -11,24 +11,58 @@ class StudentController extends Controller {
 		$this->display();//加载当前函数的模板文件
 	}
 	public function chinese(){
-		$id = isset($_GET['id']) ? intval($_GET['id']) : '';//获取网址链接id
-		if($id !== ''){
-			$homeworkModel = M("homework");//获取表homework
-			$homeworkResult = $homeworkModel->where("subjectid=".$id)->find();//.为连接符
-			$this->assign('course',$homeworkResult);//赋值
-		}
-		
-
-		layout(false); // 临时关闭当前模板的布局功能
-		$this->display();//加载当前函数的模板文件
+		public function chinese($id=''){
+ +		//获取我的信息
+ +		$studentModel = M("student");
+ +		$tel = session('tel');
+ +		$currentUser = $studentModel->where("tel=".$tel)->select();
+ +		$this->assign("current",$currentUser[0]);
+ +
+  		$id = isset($_GET['id']) ? intval($_GET['id']) : '';//获取网址链接id
+ +		$homeworkModel = M("homework");//获取表homework
+  		if($id !== ''){
+ -			$homeworkModel = M("homework");//获取表homework
+ -			$homeworkResult = $homeworkModel->limit("3")->where("subjectid=".$id)->select();//.为连接符
+ +			$homeworkResult = $homeworkModel->limit("3")->where("subjectid=".$id)->order("homeworkid desc")->select();//.为连接符
+  			$homeworkSubject = $homeworkModel->where("subjectid=".$id)->select();
+  			$this->assign("subject",$homeworkSubject[0]);
+  			$this->assign('courses',$homeworkResult);//赋值
+ +		}else{
+ +			$homeworkResult = $homeworkModel->limit("3")->where("subjectid=1")->select();//.为连接符
+ +			$homeworkSubject = $homeworkModel->where("subjectid=1")->select();
+ +			$this->assign("subject",$homeworkSubject[0]);
+ +			$this->assign('courses',$homeworkResult);//赋值
+  		}
+  		
+ -		//获取我的信息
+ -		$studentModel = M("student");
+ -		$tel = session('tel');
+ -		$currentUser = $studentModel->where("tel=".$tel)->select();
+ -		$this->assign("current",$currentUser[0]);
+  
+  		layout(false); // 临时关闭当前模板的布局功能
+  		$this->display();//加载当前函数的模板文件
 	}
 	public function classgarden(){
 		$homeworkModel = M("homework");//获取表homework
-		$homeworkResult = $homeworkModel->where("subjectid=5")->find();//.为连接符
-		$this->assign('course',$homeworkResult);//赋值
+ -		$homeworkResult = $homeworkModel->where("subjectid=5")->find();//.为连接符
+ -		$this->assign('course',$homeworkResult);//赋值
+ -
+  		$studentModel = M("student");
+  		//获取我的信息
+  		$tel = session('tel');
+  		$currentUser = $studentModel->where("tel=".$tel)->select();
+  		$this->assign("current",$currentUser[0]);
+  
+ +		$resourceModel = M("resource");//获取表resource
+ +		$resourceResult = $resourceModel->limit("3")->order("id desc")->select();//.为连接符
+ +		$this->assign('resource',$resourceResult);//赋值
+ +
+  		layout(false); // 临时关闭当前模板的布局功能
+  		$this->display();//加载当前函数的模板文件
+  	}
 
-		layout(false); // 临时关闭当前模板的布局功能
-		$this->display();//加载当前函数的模板文件
+  		$this->display();//加载当前函数的模板文件
 	}
 	public function classspace(){
 		$classspaceModel =  M("classspace");//获取表classspace
